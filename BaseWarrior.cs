1393436fd2ece8211fc3e34fb376c51e9f56e57e
RealFoxerity <93208650+RealFoxerity@users.noreply.github.com>
2021-10-26 17:19:39 +0200

    cheesecake

---
+++ BaseWarrior.cs
@@ -0,0 +1,38 @@
+public class BaseWarrior : MonoBehaviour {
+    private string baseWarrior;
+    private int damage;
+    private double health;
+    private int agility;
+    private int strenght;
+    private int inteligence;
+
+    public string BaseWarrior 
+    {
+        get {return BaseWarrior;}
+        set {BaseWarrior = Warrior;}
+    }
+    public int Damage
+    {
+        get {return damage;}
+        set {damage = 69;}
+    }
+    public double Health
+    {
+        get {return health;}
+        set {health = 120;}
+    }
+    public int Agility{
+        get {return agility;}
+        set {agility = 10;}
+    }
+    public int Strength{
+        get {return strength;}
+        set {strength = 48;}
+    }
+    public int Inteligence {
+        get {return inteligence;}
+        set {inteligence = 8;}
+    }
+
+
+}
