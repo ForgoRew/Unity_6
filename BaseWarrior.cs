c06e49e78b1f826c8c881fa69d12164cb7dbf52d
ForgoRew <milos.halda@gmail.com>
2021-10-26 17:29:03 +0200

    Warrior changes.

--- BaseWarrior.cs
+++ BaseWarrior.cs
@@ -1,38 +1,5 @@
-public class BaseWarrior {
-    private string baseWarrior;
-    private int damage;
-    private double health;
-    private int agility;
-    private int strength;
-    private int inteligence;
-
-    public string BaseWarrior 
-    {
-        get {return baseWarrior;}
-        set {baseWarrior = Warrior;}
+public class BaseWarrior : BaseClassScript {
+    public BaseWarrior(){
+        ClassName = "Warrior";
     }
-    public int Damage
-    {
-        get {return damage;}
-        set {damage = 69;}
-    }
-    public double Health
-    {
-        get {return health;}
-        set {health = 120;}
-    }
-    public int Agility{
-        get {return agility;}
-        set {agility = 10;}
-    }
-    public int Strength{
-        get {return strength;}
-        set {strength = 48;}
-    }
-    public int Inteligence {
-        get {return inteligence;}
-        set {inteligence = 8;}
-    }
-
-
 }
