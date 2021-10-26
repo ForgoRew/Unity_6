4c2c082188c7b36c8965eefe59c0c80be9077c00
ForgoRew <milos.halda@gmail.com>
2021-10-26 16:52:59 +0200

    ...

--- BaseClassScript.cs
+++ BaseClassScript.cs
@@ -1,6 +1,11 @@
 public class BaseClassScript {
     private string className;
     private int damage;
+    private double health;
+    private int agility;
+    private int strenght;
+    private int inteligence;
+
     public string ClassName 
     {
         get {return className;}
@@ -11,5 +16,23 @@ public class BaseClassScript {
         get {return damage;}
         set {damage = value;}
     }
+    public double Health
+    {
+        get {return health;}
+        set {health = value;}
+    }
+    public int Agility{
+        get {return agility;}
+        set {agility = value;}
+    }
+    public int Strength{
+        get {return strength;}
+        set {strength = value;}
+    }
+    public int Inteligence {
+        get {return inteligence;}
+        set {inteligence = value;}
+    }
+
 
 }
