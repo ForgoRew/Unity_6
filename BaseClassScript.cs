930b8217f742cb4ff49d03127338b9b27c3a587e
ForgoRew <milos.halda@gmail.com>
2021-10-26 15:11:15 +0200

    Prep for lecture.

--- BaseClassScript.cs
+++ BaseClassScript.cs
@@ -1,9 +1,15 @@
 public class BaseClassScript {
     private string className;
+    private int damage;
     public string ClassName 
     {
         get {return className;}
         set {className = value;}
     }
-    
+    public int Damage
+    {
+        get {return damage;}
+        set {damage = value;}
+    }
+
 }
---
+++ BaseNewbie.cs
@@ -0,0 +1,6 @@
+public class BaseNewbie : BaseClassScript {
+    public BaseNewbie() {
+        ClassName = "Newbie";
+        Damage = 1;
+    }
+}
