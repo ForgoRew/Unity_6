69c2bea400012f8221c678537d135a1fc0bb0aa9
coldyGit <krizekf66@gmail.com>
2021-10-26 16:54:18 +0200

    lul

--- BaseClassScript.cs
+++ BaseClassScript.cs
@@ -1,7 +1,12 @@
 public class BaseClassScript {
     private string className;
     private int damage;
-    public string ClassName 
+    private double health;
+    private int agility;
+    private int strenght;
+    private int inteligence;
+
+    public string ClassName;
     {
         get {return className;}
         set {className = value;}
@@ -11,5 +16,23 @@ public class BaseClassScript {
         get {return damage;}
         set {damage = value;}
     }
-
+    public double health{
+        get {return health;}
+        set {set health;}
+    }
+    public int agility
+    {
+        get {return agility;}
+        set {set agility;}
+    }
+    public int strenght
+    {
+        get {return agility;}
+        set {set agility;}
+    }
+    public int inteligence
+    {
+        get {return agility;}
+        set {set agility;}
+    }
 }
