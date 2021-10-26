60b0155d63cb3342309f263754f50fa10a692a96
coldyGit <krizekf66@gmail.com>
2021-10-26 17:55:21 +0200

    neco

--- BaseSummoner.cs
+++ BaseSummoner.cs
@@ -2,10 +2,13 @@ public class BaseSummoner : BaseClassScript {
 
     private string summonAbility;
 
+    public string summonSummoned(){
+        return "neydd 5 mana for zumun";
+    }
     public string summonAbility
     {
         get {return summonAbility;}
-        set {summonAbility=xd;}
+        set {summonAbility=value;}
     }
     public BaseSummoner() {
         ClassName = "Summoner";
@@ -14,5 +17,6 @@ public class BaseSummoner : BaseClassScript {
         Agility = 3;
         Strenght = 4;
         Inteligence = 3;
+        SummonAbility = "ghost";
     }
 }
