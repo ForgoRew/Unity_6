30d45adea639527627a75acd965a9a62218c67b6
coldyGit <krizekf66@gmail.com>
2021-10-26 17:02:29 +0200

    lul

---
+++ BaseArcher.cs
@@ -0,0 +1,9 @@
+public class BaseArcher : BaseClassScript {
+    public BaseArcher() {
+        ClassName = "Archer";
+        Damage = 1;
+        Health = 5;
+        Agility = 1;
+        Strength = 
+    }
+}
